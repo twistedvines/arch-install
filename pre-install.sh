8b9ae9dbe429636b7bf095397e1e00d7c883bb67
Harry Owen <harry@twistedvin.es>
2017-11-05 18:38:04 +0000

    Use env var for target install volume

--- pre-install.sh
+++ pre-install.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
 
+if [ -n "$ARCH_INSTALL_DISK" ]; then
+  target_volume="$ARCH_INSTALL_DISK"
+else
+  target_volume='/dev/sda'
+fi
+
 create_disk() {
-  parted -s /dev/sda mklabel msdos \
+  parted -s "$target_volume" mklabel msdos \
     mkpart primary 512KiB 300MiB set 1 boot on \
     mkpart primary 300MiB 10% \
     mkpart primary 10% 25% \
     mkpart primary 25% 100%
 
-  mkfs.ext2 /dev/sda1
-  mkfs.ext4 /dev/sda2
-  mkfs.ext4 /dev/sda3
-  mkfs.ext4 /dev/sda4
+  mkfs.ext2 "${target_volume}1"
+  mkfs.ext4 "${target_volume}2"
+  mkfs.ext4 "${target_volume}3"
+  mkfs.ext4 "${target_volume}4"
 }
 
 # sync-up time
@@ -21,10 +27,10 @@ echo "creating disk..."
 create_disk
 
 echo "mounting filesystems..."
-mount /dev/sda2 /mnt
+mount "${target_volume}2" /mnt
 mkdir /mnt/var
-mount /dev/sda3 /mnt/var
+mount "${target_volume}3" /mnt/var
 mkdir /mnt/home
-mount /dev/sda4 /mnt/home
+mount "${target_volume}4" /mnt/home
 mkdir /mnt/boot
-mount /dev/sda1 /mnt/boot
+mount "${target_volume}1" /mnt/boot
